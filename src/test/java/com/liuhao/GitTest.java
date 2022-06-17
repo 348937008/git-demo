39a731d1f3c82a10f3eee33cf060186a9d9b47a3
tom <348937008@qq.com>
2022-06-17 12:52:33 +0800

    master test commit

--- src/test/java/com/liuhao/GitTest.java
+++ src/test/java/com/liuhao/GitTest.java
@@ -6,5 +6,7 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("Hello Git");
         System.out.println("Hello Git4");
+        System.out.println("master test");
     }
+
 }
